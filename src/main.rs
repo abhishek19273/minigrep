9dc748a32b81cd6b357545729c93a2d6a879aa5e
abhishek19273 <devabhishek81121@gmail.com>
2025-12-28 12:02:10 +0530

    chore : Improved Error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use std::env;
+use std::{env, process};
 mod parse;
 mod read_file;
-use parse::{Config, parse_config};
+use parse::Config;
 use read_file::read_file_content;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config: Config = parse_config(&args);
+    let config: Config = match Config::build(&args) {
+        Ok(val) => val,
+        Err(s) => {
+            println!("{s}");
+            process::exit(1);
+        }
+    };
     println!("The pattern to search for is {}", config.query);
     println!("The file to search this pattern is {}", config.file_name);
     let file_content = read_file_content(&config.file_name);
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -5,19 +5,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Self {
+    pub fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("Not Enough Arguments.");
+        }
         let query = args
             .get(1)
             .expect("Please provide the query to search in the file name correctly.");
         let file_name = args
             .get(2)
             .expect("Please provide the file_name correctly to search for the query.");
-        Self {
+        Ok(Self {
             query: query.clone(),
             file_name: file_name.clone(),
-        }
+        })
     }
-}
-pub fn parse_config(args: &[String]) -> Config {
-    Config::new(args)
 }
