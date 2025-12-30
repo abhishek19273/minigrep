d7dcf5afe862dd836f3603a3e1f21b93400689ea
abhishek19273 <devabhishek81121@gmail.com>
2025-12-30 21:33:15 +0530

    chore : Implemented `search` in library

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,24 @@
 pub fn search<'a>(query: &str, file_content: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+    let mut results = Vec::new();
+    for line in file_content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
+#[cfg(test)]
+mod test {
+    use crate::search;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let file_content = "\
+Rust:
+safe, fast, productive
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive"], search(query, file_content));
+    }
+}
