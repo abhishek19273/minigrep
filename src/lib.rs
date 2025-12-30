c2fe79374729e45cd863e890eb04501b4aacbc27
abhishek19273 <devabhishek81121@gmail.com>
2025-12-30 21:44:39 +0530

    chore : Added search case insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,19 @@ pub fn search<'a>(query: &str, file_content: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, file_content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in file_content.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 #[cfg(test)]
 mod test {
-    use crate::search;
+    use crate::{search, search_case_insensitive};
 
     #[test]
     fn one_result() {
@@ -20,5 +30,18 @@ Rust:
 safe, fast, productive
 Pick three.";
         assert_eq!(vec!["safe, fast, productive"], search(query, file_content));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "DuCt";
+        let file_content = "\
+Rust:
+safe, fast, prodUcTive
+Pick three.";
+        assert_eq!(
+            vec!["safe, fast, prodUcTive"],
+            search_case_insensitive(query, file_content)
+        );
     }
 }
